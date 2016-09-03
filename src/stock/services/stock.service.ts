f0b4708d984e51d5b861ffeb6a352570eb140703
kwintenp <kwinten.pisman@gmail.com>
2016-09-03 10:16:27 +0200

    Instead of subscribing every time the json web token is requested. Just subscribe to it and use the json web token as a private variable.

--- src/stock/services/stock.service.ts
+++ src/stock/services/stock.service.ts
@@ -10,8 +10,12 @@ import {addWine, addAllWines} from "../../statemanagement/actionCreators";
 
 @Injectable()
 export class StockService {
+    private jwtToken: string;
+
     constructor(private store: Store<ApplicationState>,
                 private http: Http) {
+        this.store.select((state: ApplicationState) => state.data.authentication.jwtToken)
+            .subscribe((jwtToken: string) => this.jwtToken = jwtToken);
     }
 
     add(wine: Wine): void {
@@ -54,10 +58,8 @@ export class StockService {
     }
 
     private authorizedHttpOptions(): RequestOptionsArgs {
-        let state: ApplicationState;
-        this.store.take(1).subscribe(s => state = s);
         let headers = new Headers({
-            authorization: `Bearer ${state.data.authentication.jwtToken}`
+            authorization: `Bearer ${this.jwtToken}`
         });
         return new RequestOptions({headers: headers});
     }
