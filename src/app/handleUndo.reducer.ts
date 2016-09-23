5226da525cd6d3906eaa78667f9f4570dbf40ecb
Brecht Billiet <billietbrecht@gmail.com>
2016-09-23 15:21:23 +0200

    handle undo reducer

--- src/app/handleUndo.reducer.ts
+++ src/app/handleUndo.reducer.ts
@@ -1,15 +1,24 @@
 import {ApplicationState} from "../statemanagement/state/ApplicationState";
 import {Action, ActionReducer} from "@ngrx/store";
-export function handleUndo(rootReducer: ActionReducer<ApplicationState>): ActionReducer<ApplicationState> {
+export function handleUndo(rootReducer: ActionReducer<ApplicationState>, bufferSize = 100): ActionReducer<ApplicationState> {
     let executedActions: Array<Action> = [];
+    let initialState = undefined;
     return (state: ApplicationState, action: Action) => {
         if (action.type === "UNDO_ACTION") {
-            let newState: any = {};
+            let newState: any = initialState;
             executedActions = executedActions.filter(eAct => eAct !== action.payload);
             executedActions.forEach(executedAction => newState = rootReducer(newState, executedAction));
             return newState;
         }
         executedActions.push(action);
-        return rootReducer(state, action);
+        let updatedState =  rootReducer(state, action);
+        if (executedActions.length === bufferSize + 1) {
+            let firstAction = executedActions[0];
+            // calculate the state x (buffersize) actions ago
+            initialState = rootReducer(initialState, firstAction);
+            // keep the correct actions
+            executedActions = executedActions.slice(1, bufferSize + 1);
+        }
+        return updatedState;
     };
 }
